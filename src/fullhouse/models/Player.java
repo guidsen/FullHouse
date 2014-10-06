c3cf68c4d8496230480933afb2840eeaf1fbcecd
guidsen <guisch95@gmail.com>
2014-10-06 16:56:36 +0200

    Added Player model

---
+++ src/fullhouse/models/Player.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fullhouse.models;
+
+import fullhouse.Eloquent;
+
+/**
+ *
+ * @author Guido
+ */
+public class Player extends Eloquent {
+    protected String table = "player";
+}
