041f7b43768c41d51ddf4d33d35091f816782fe8
Guido Schmitz <guisch95@gmail.com>
2014-10-07 19:24:21 +0200

    Added methods for getting column names of the model

--- src/fullhouse/repositories/DbRepository.java
+++ src/fullhouse/repositories/DbRepository.java
@@ -5,23 +5,40 @@
  */
 package fullhouse.repositories;
 
+import fullhouse.DataSource;
+import java.sql.*;
+import java.util.HashMap;
+
 /**
  *
  * @author Guido
  * @param <T>
  */
-public class DbRepository<T> implements DbRepositoryInterface<T>{
+public abstract class DbRepository<T> implements DbRepositoryInterface<T> {
 
     protected T model;
 
+    public abstract String getInsertString();
+
+    protected abstract String[] getColumnNames();
+
     protected DbRepository(T model) {
-        System.out.println("DbRepository created.");
         this.model = model;
-        System.out.println(model);
+    }
+
+    public HashMap<Integer, String> getColumns() {
+        HashMap<Integer, String> columnsMap = new HashMap<>();
+        for (int i = 0; i < getColumnNames().length; i++) {
+            columnsMap.put(i + 1, getColumnNames()[i]);
+        }
+        return columnsMap;
     }
 
     @Override
     public T getModel() {
         return model;
+    }
+
+    public void add(T model) throws Exception {
     }
 }
