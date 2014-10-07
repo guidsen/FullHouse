55f3f3f6e5e61019cff617401fe58286e5385880
Guido Schmitz <guisch95@gmail.com>
2014-10-07 04:29:53 +0200

    Boot HomeFrame in main

--- src/fullhouse/FullHouse.java
+++ src/fullhouse/FullHouse.java
@@ -18,15 +18,8 @@ public class FullHouse {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Integer[] players = {1560, 1400, 977, 632, 1274};
-        
-        int total = 0;
-        for(int player : players) {
-            total += player;
-        }
-        
-        Rating rating = new Rating(total, 6, 2);
-        System.out.println(rating.calculate());
+        JFrame frame = new HomeFrame();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
     }
-
 }
