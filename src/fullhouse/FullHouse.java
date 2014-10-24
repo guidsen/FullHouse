850e5013123d55f7fca3e8d7d6acb3700a7a952c
Guido Schmitz <guisch95@gmail.com>
2014-10-24 03:46:08 +0200

    Added helper functions.

--- src/fullhouse/FullHouse.java
+++ src/fullhouse/FullHouse.java
@@ -6,6 +6,7 @@
 package fullhouse;
 
 import fullhouse.modules.HomeFrame;
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import javax.swing.JFrame;
@@ -21,46 +22,65 @@ public class FullHouse {
      */
     public static void main(String[] args) {
         HomeFrame frame = new HomeFrame();
-        
+
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
         frame.revalidate();
 
         frame.mainPanel = Panel.changeView(frame, frame.mainPanel, new fullhouse.modules.HomePanel());
-        
+
         /*System.out.println("Punten op 500");
-        for(int i = 5; i <= 20; i++)
-        {
-            new Rating(500, i*100);
-        }
-        System.out.println("------------------");
-        System.out.println("Punten op 1000");
-        for(int i = 5; i <= 20; i++)
-        {
-            new Rating(1000, i*100);
-        }
-        System.out.println("------------------");
-        System.out.println("Punten op 2000");
-        for(int i = 5; i <= 20; i++)
-        {
-            new Rating(2000, i*100);
-        }*/
+         for(int i = 5; i <= 20; i++)
+         {
+         new Rating(500, i*100);
+         }
+         System.out.println("------------------");
+         System.out.println("Punten op 1000");
+         for(int i = 5; i <= 20; i++)
+         {
+         new Rating(1000, i*100);
+         }
+         System.out.println("------------------");
+         System.out.println("Punten op 2000");
+         for(int i = 5; i <= 20; i++)
+         {
+         new Rating(2000, i*100);
+         }*/
     }
-    
+
     public static Timestamp toSqlDate(int year, int month, int day, int hour, int minute) {
         java.util.Calendar cal = java.util.Calendar.getInstance();
         cal.clear();
-        
+
         cal.set(java.util.Calendar.YEAR, year);
-        cal.set(java.util.Calendar.MONTH, month-1);
+        cal.set(java.util.Calendar.MONTH, month - 1);
         cal.set(java.util.Calendar.DAY_OF_MONTH, day);
         cal.set(java.util.Calendar.HOUR_OF_DAY, hour);
         cal.set(java.util.Calendar.MINUTE, minute);
-        
+
         return new Timestamp(cal.getTimeInMillis());
     }
-    
+
+    public static String textToSqlDate(String dateString) {
+        String day = dateString.substring(0, 2);
+        String month = dateString.substring(3, 5);
+        String year = dateString.substring(6, 10);
+        return year + "-" + month + "-" + day;
+    }
+
     public static String fromSqlDate(Timestamp date) {
         return new SimpleDateFormat("dd-MM-YYYY HH:mm").format(date);
+    }
+
+    public static String fromSqlDate(Date date) {
+        return new SimpleDateFormat("dd-MM-YYYY").format(date);
+    }
+
+    public static String fromSqlDate(Timestamp date, String format) {
+        return new SimpleDateFormat(format).format(date);
+    }
+
+    public static String fromSqlDate(Date date, String format) {
+        return new SimpleDateFormat(format).format(date);
     }
 }
