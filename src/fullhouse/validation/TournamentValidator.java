25bcdd41c57dd6e8aa666e273c2b54ffb2f2bbe1
Guido Schmitz <guisch95@gmail.com>
2014-10-27 13:51:49 +0100

    n/a

---
+++ src/fullhouse/validation/TournamentValidator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fullhouse.validation;
+
+import fullhouse.exceptions.FormValidationException;
+import fullhouse.modules.tournament.TournamentFormPanel;
+
+/**
+ *
+ * @author Guido
+ */
+public class TournamentValidator {
+
+    public void validate(TournamentFormPanel form) throws FormValidationException {
+        FormValidator validator = FormValidator.getInstance();
+        validator.resetErrors();
+        System.out.println(form.nameField.getText());
+        validator.addTextField(form.nameField, form.nameValidationLabel).required();
+
+        if (validator.getErrorCount() > 0) {
+            throw new FormValidationException(validator.getErrors());
+        }
+    }
+}
