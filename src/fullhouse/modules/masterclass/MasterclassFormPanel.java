a12eeb8625d49cf150f07bfce54ec146fe9cdf63
steve <steve@PC-steve.wireless.hhs.nl>
2014-10-31 17:57:23 +0100

    Add MasterclassFormPanel change

--- src/fullhouse/modules/masterclass/MasterclassFormPanel.java
+++ src/fullhouse/modules/masterclass/MasterclassFormPanel.java
@@ -51,9 +51,11 @@ public class MasterclassFormPanel extends javax.swing.JPanel {
         Player selectedLeader = (Player) leaderIdBox.getSelectedItem();
         Masterclass masterclass = new Masterclass();
         masterclass.setLeaderId(selectedLeader.getId());
-        masterclass.setMinRating(Integer.parseInt(minRatingField.getText()));
-        masterclass.setName(nameField.getText());
-        masterclass.setDate(dateField.getText());
+        masterclass.setMinRating(Integer.parseInt(masterclassMinRating.getText())); 
+        masterclass.setPrice(Double.parseDouble(masterclassPrice.getText()));
+        masterclass.setName(masterclassName.getText());
+        masterclass.setMaxPlayers(Integer.parseInt(masterclassMaxPlayers.getText()));
+        masterclass.setDate(masterclassDate.getText());
         
         return masterclass;
     }
@@ -63,9 +65,11 @@ public class MasterclassFormPanel extends javax.swing.JPanel {
         Masterclass masterclass = new Masterclass();
         masterclass.setId(id);
         masterclass.setLeaderId(selectedLeader.getId());
-        masterclass.setMinRating(Integer.parseInt(minRatingField.getText()));
-        masterclass.setName(nameField.getText());
-        masterclass.setDate(dateField.getText());
+        masterclass.setMinRating(Integer.parseInt(masterclassMinRating.getText()));
+        masterclass.setPrice(Double.parseDouble(masterclassPrice.getText()));
+        masterclass.setName(masterclassName.getText());
+        masterclass.setMaxPlayers(Integer.parseInt(masterclassMaxPlayers.getText()));
+        masterclass.setDate(masterclassDate.getText());
         
         return masterclass;
     }
@@ -73,9 +77,11 @@ public class MasterclassFormPanel extends javax.swing.JPanel {
     public void setValues() {
         System.out.println(this.masterclass.getLeaderId());
         leaderIdBox.setSelectedItem(Integer.toString(this.masterclass.getLeaderId()));
-        minRatingField.setText(Integer.toString(this.masterclass.getMinRating()));
-        nameField.setText(this.masterclass.getName());
-        dateField.setText(this.masterclass.getDate());
+        masterclassMinRating.setText(Integer.toString(this.masterclass.getMinRating()));
+        masterclassPrice.setText(Double.toString(this.masterclass.getPrice()));
+        masterclassName.setText(this.masterclass.getName());
+        masterclassMaxPlayers.setText(Integer.toString(this.masterclass.getMaxPlayers()));
+        masterclassDate.setText(this.masterclass.getDate());
     }
 
     /**
@@ -88,17 +94,20 @@ public class MasterclassFormPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        nameField = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        dateField = new javax.swing.JTextField();
+        masterclassDate = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        minRatingField = new javax.swing.JTextField();
-        jLabel7 = new javax.swing.JLabel();
+        masterclassMinRating = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        masterclassPrice = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        masterclassMaxPlayers = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        leaderIdBox = new javax.swing.JComboBox();
         nameValidationLabel = new javax.swing.JLabel();
         dateValidationLabel = new javax.swing.JLabel();
-        minRatingValidationLabel = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        leaderIdBox = new javax.swing.JComboBox();
+        masterclassName = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
 
         jLabel1.setText("Naam");
 
@@ -106,15 +115,15 @@ public class MasterclassFormPanel extends javax.swing.JPanel {
 
         jLabel3.setText("Min rating");
 
+        jLabel5.setText("Prijs");
+
+        jLabel4.setText("Max spelers");
+
+        jLabel6.setText("Gegeven door");
+
+        leaderIdBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
         jLabel7.setText("(dd-mm-jjjj uu:mm)");
-
-        jLabel4.setText("Gegeven door");
-
-        leaderIdBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                leaderIdBoxActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -122,54 +131,64 @@ public class MasterclassFormPanel extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 87, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 87, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
+                        .addComponent(jLabel5))
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel6))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(nameField)
-                    .addComponent(dateField)
-                    .addComponent(minRatingField, javax.swing.GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
-                    .addComponent(leaderIdBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(masterclassDate)
+                    .addComponent(masterclassPrice)
+                    .addComponent(masterclassMinRating)
+                    .addComponent(masterclassMaxPlayers)
+                    .addComponent(leaderIdBox, 0, 130, Short.MAX_VALUE)
+                    .addComponent(masterclassName))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(nameValidationLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel7)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(dateValidationLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 254, Short.MAX_VALUE))
-                    .addComponent(nameValidationLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(minRatingValidationLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                        .addComponent(dateValidationLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(dateValidationLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(nameValidationLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(masterclassName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(nameValidationLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                .addComponent(jLabel1)
-                                .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
-                            .addComponent(dateField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel7))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(minRatingField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(minRatingValidationLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(leaderIdBox)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                            .addComponent(masterclassDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(dateValidationLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(masterclassPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(masterclassMinRating, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(masterclassMaxPlayers, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(leaderIdBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jLabel7))
+                .addContainerGap(139, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -178,17 +197,20 @@ public class MasterclassFormPanel extends javax.swing.JPanel {
     }//GEN-LAST:event_leaderIdBoxActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public javax.swing.JTextField dateField;
     public javax.swing.JLabel dateValidationLabel;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    public javax.swing.JLabel jLabel7;
-    public javax.swing.JComboBox leaderIdBox;
-    public javax.swing.JTextField minRatingField;
-    public javax.swing.JLabel minRatingValidationLabel;
-    public javax.swing.JTextField nameField;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JComboBox leaderIdBox;
+    public javax.swing.JTextField masterclassDate;
+    public javax.swing.JTextField masterclassMaxPlayers;
+    public javax.swing.JTextField masterclassMinRating;
+    public javax.swing.JTextField masterclassName;
+    public javax.swing.JTextField masterclassPrice;
     public javax.swing.JLabel nameValidationLabel;
     // End of variables declaration//GEN-END:variables
 }
